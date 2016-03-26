3ed882bd54ab5bec77bcb0465853b1de0787224b
Daneyon Hansen <danehans@cisco.com>
2016-03-26 06:44:09 +0000

    Updates Ansible setup.sh playbook/inventory variable defs

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-inventory=${INVENTORY:-inventory}
-playbook=${PLAYBOOK:-playbooks/gem-master.yml}
+inventory=${INVENTORY:-inventory/gemini-master}
+playbook=${PLAYBOOK:-playbooks/gemini-master.yml}
 
 ansible-playbook -i ${inventory} ${playbook} $@
