0aaf71b5abede8e659e1df223e0fa41a5a76f8c8
stelouie <stelouie@cisco.com>
2016-04-05 17:27:46 -0400

    Support for gemini_master downloads instead of fetching binaries and tars from the internet. Includes new gemini_master golden image setup instructions

--- vagrant/start_scripts/get_kube.sh
+++ vagrant/start_scripts/get_kube.sh
@@ -1,3 +1,19 @@
-wget https://github.com/kubernetes/kubernetes/releases/download/v$kube_version/kubernetes.tar.gz
-mkdir -p ../provision_files/html/downloads/bins/kubernetes/v$kube_version
-mv kubernetes.tar.gz  ../provision_files/html/downloads/bins/kubernetes/v$kube_version/
+kube_version=1.2.0
+
+binaries="kube-apiserver 
+kube-controller-manager 
+kube-proxy 
+kube-scheduler 
+kubectl 
+kubelet"
+
+kube_download_url_base="https://storage.googleapis.com/kubernetes-release/release/v$kube_version/bin/linux/amd64/"
+
+mkdir -p ../provision_files/html/downloads/bins/kubernetes/v$kube_version/
+
+for b in $binaries
+do 
+  echo $kube_download_url_base$b
+  wget $kube_download_url_base$b
+  mv $b ../provision_files/html/downloads/bins/kubernetes/v$kube_version/
+done
