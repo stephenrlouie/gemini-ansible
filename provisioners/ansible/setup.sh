b17bfd11cbbad35eba99eb0a7d94f45717a9ebf0
Daneyon Hansen <danehans@cisco.com>
2016-04-08 09:42:05 +0000

    Updates Ansible for Install Doc Fixes
    Previously, the installation guide and gemini ansible code
    was out of sync. This commit provides several updates to sync
    the two.

--- provisioners/ansible/setup.sh
+++ provisioners/ansible/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-inventory=${INVENTORY:-inventory/gemini-master}
-playbook=${PLAYBOOK:-playbooks/gemini-master.yml}
+inventory=${INVENTORY:-gemini-master}
+playbook=${PLAYBOOK:-gemini-master.yml}
 
 ansible-playbook -i ${inventory} ${playbook} $@
