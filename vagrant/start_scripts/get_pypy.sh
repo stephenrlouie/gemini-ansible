ae61e9476f98baae5516de40b69fa0ffa4f93b89
Steve Louie <stephen.r.louie@gmail.com>
2016-04-11 16:14:58 -0400

    Typo on download destination

--- vagrant/start_scripts/get_pypy.sh
+++ vagrant/start_scripts/get_pypy.sh
@@ -1,3 +1,3 @@
 wget https://bitbucket.org/pypy/pypy/downloads/pypy-$pypy_version-linux64.tar.bz2
 mkdir -p ../provision_files/html/releases/pypy/v$pypy_version
-mv pypy-$pypy_version-linux64.tar.bz2 ../provision_files/html/releasses/pypy/v$pypy_version
+mv pypy-$pypy_version-linux64.tar.bz2 ../provision_files/html/releases/pypy/v$pypy_version/
